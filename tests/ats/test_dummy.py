7d0692b0ce9fcea310718366ed9b80835217ad4f
Łukasz Piątkowski <piontec@gmail.com>
2022-06-22 12:29:26 +0200

    fixture for GS CRDs

--- tests/ats/test_dummy.py
+++ tests/ats/test_dummy.py
@@ -1,3 +1,4 @@
+import logging
 from typing import Callable
 
 import pytest
@@ -8,7 +9,9 @@ from pytest_helm_charts.k8s.deployment import wait_for_deployments_to_run
 FLUX_VERSION = "0.11.0"
 FLUX_NAMESPACE_NAME = "default"
 FLUX_DEPLOYMENTS_READY_TIMEOUT: int = 180
+GS_CRDS_COMMIT_URL = "https://raw.githubusercontent.com/giantswarm/apiextensions/15836a106059cc8d201e1237adf44aec340bbab6/helm/crds-common/templates/giantswarm.yaml"
 
+logger = logging.getLogger(__name__)
 
 @pytest.fixture
 def flux_app_deployment(kube_cluster: Cluster, app_factory: AppFactoryFunc) -> ConfiguredApp:
@@ -28,6 +31,17 @@ def flux_app_deployment(kube_cluster: Cluster, app_factory: AppFactoryFunc) -> C
         FLUX_DEPLOYMENTS_READY_TIMEOUT,
     )
     return flux_app
+
+
+@pytest.fixture
+def gs_crds(kube_cluster: Cluster) -> None:
+    yield kube_cluster.kubectl(f"create -f {GS_CRDS_COMMIT_URL}")
+    kube_cluster.kubectl(f"delete -f {GS_CRDS_COMMIT_URL}")
+
+
+@pytest.fixture
+def flux_environment(flux_app_deployment: ConfiguredApp) -> ConfiguredApp:
+    return flux_app_deployment
 
 
 @pytest.mark.smoke
